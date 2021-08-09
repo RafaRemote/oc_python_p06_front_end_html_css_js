fbc83e117f662cf75db636c0e6243883e3d0d4ad
rafaRemote <raphael.49410@gmail.com>
2021-08-09 07:59:33 +0200

    Enhanced js (checked with linter). Enhancement to be continued

--- main.js
+++ main.js
@@ -16,30 +16,34 @@ function build() {
     let wrapper = document.getElementById("containers_wrapper");
     categories.sort().reverse();
     for (i in categories) {
-        div = document.createElement('div')
-        div.classList.add('container_for_seven')
-        div_title = document.createElement('div')
-        div_title.classList.add('container__title')
-        p_title = document.createElement('p')
-        p_title.innerText = categories[i][0].toUpperCase() + categories[i].slice(1)
-        div_title.appendChild(p_title)
-        div.appendChild(div_title)
-        div_movies = document.createElement('div')
-        div_movies.classList.add('container__movies')
-        div_movies.id = 'div_container_movies_' + categories[i]
-        div.appendChild(div_movies)
-        div.id = 'div_'+ categories[i]
-        wrapper.appendChild(div)
+        div = document.createElement("div");
+        div.classList.add("container_for_seven");
+        div_title = document.createElement("div");
+        div_title.classList.add("container__title");
+        p_title = document.createElement("p");
+        p_title.innerText = categories[i][0].toUpperCase() +
+                            categories[i].slice(1);
+        div_title.appendChild(p_title);
+        div.appendChild(div_title);
+        div_movies = document.createElement("div");
+        div_movies.classList.add("container__movies");
+        div_movies.id = "div_container_movies_" + categories[i];
+        div.appendChild(div_movies);
+        div.id = "div_"+ categories[i];
+        wrapper.appendChild(div);
     };
 }
 
 function launch() {
     for (i in categories.slice(1)) {
-        list_urls.push([url_base.slice(0, 36)+'?genre='+categories[i]+'&'+url_base.slice(-19), categories[i]])
-    }
+        list_urls.push([url_base.slice(0, 36)+"?genre=" +
+                        categories[i] +
+                        "&" +
+                        url_base.slice(-19), categories[i]])
+    };
     for (i in list_urls) {
         getData(list_urls[i][0], list_urls[i][1])
-    }
+    };
 }
 
 function getData(url, cat) {
@@ -73,14 +77,14 @@ function getBest(data) {
 function dispatch_best(data) {
     let title = document.getElementById("best_movie_all_cat_title")
     let img = document.getElementById("img_best")
-    let container = document.getElementsByClassName('container__bestMovie__title')[0]
-    let short_description = document.createElement('p')
+    let container = document.getElementsByClassName("container__bestMovie__title")[0]
+    let short_description = document.createElement("p")
     img.id = data.id
     img.src = data.image_url
-    img.classList.add('a_bit_bigger')
+    img.classList.add("a_bit_bigger")
     title.textContent = data.title
-    short_description.innerText = 'Résumé: ' + data.description
-    short_description.style.fontStyle = 'italic'
+    short_description.innerText = "Résumé: " + data.description
+    short_description.style.fontStyle = "italic"
     container.appendChild(short_description)
     listen_click(img.id)
 }
@@ -90,52 +94,52 @@ function dispatch(results, cat) {
     if (cat == undefined) {
         cat = categories.sort()[0]
     } 
-    let present_div = document.getElementById('div_container_movies_' + cat.toLowerCase())
+    let present_div = document.getElementById("div_container_movies_" + cat.toLowerCase())
     if (present_div.children.length > 0) {
         for (let i=1; i<5; i++) {
             present_div.children[1].remove()
         }
         for (let i in results.slice(0, 4)) {
-            let image = document.createElement('img')
+            let image = document.createElement("img")
             image.id = results[i].id
             image.src = results[i].image_url
             present_div.insertBefore(image, present_div.childNodes[present_div.childNodes.length -1])
             listen_click(image.id)
         }
     } else {
-        let div = document.getElementById('div_container_movies_' + cat.toLowerCase())
-        arrow_left = document.createElement('button')
+        let div = document.getElementById("div_container_movies_" + cat.toLowerCase())
+        arrow_left = document.createElement("button")
         arrow_left.id = "arrow_left_" + cat
-        arrow_left.innerHTML = '<i class="fas fa-arrow-right"></i>'
+        arrow_left.innerHTML = "<i class='fas fa-arrow-right'></i>"
         div.appendChild(arrow_left)
         for (let i in results.slice(0, 4)) {
-            let image = document.createElement('img')
+            let image = document.createElement("img")
             image.id = results[i].id
             image.src = results[i].image_url
             div.appendChild(image)
             listen_click(image.id)
         }
-        arrow_right = document.createElement('button')
+        arrow_right = document.createElement("button")
         arrow_right.id = "arrow_right_" + cat
-        arrow_right.innerHTML = '<i class="fas fa-arrow-left"></i>'
+        arrow_right.innerHTML = "<i class='fas fa-arrow-left'></i>"
         div.appendChild(arrow_right)
         listen_arrows(results, cat)
     }
 }
 
 function listen_arrows(movies, cat) {
-    let btn_left = document.getElementById('arrow_left_' + cat);
-    btn_left.addEventListener('click', () => {
-        setTimeout( () => {btn_left.classList.add('background_red')}, 50)
-        btn_left.classList.remove('background_red')
+    let btn_left = document.getElementById("arrow_left_" + cat);
+    btn_left.addEventListener("click", () => {
+        setTimeout( () => {btn_left.classList.add("background_red")}, 50)
+        btn_left.classList.remove("background_red")
         let element_to_move = movies.splice(-1)
         movies.splice(0, 0, element_to_move[0])
         return dispatch(movies, cat)
     })
-    let btn_right = document.getElementById('arrow_right_' + cat);
-    btn_right.addEventListener('click', () => {
-        setTimeout( () => {btn_right.classList.add('background_red')}, 50)
-        btn_right.classList.remove('background_red')
+    let btn_right = document.getElementById("arrow_right_" + cat);
+    btn_right.addEventListener("click", () => {
+        setTimeout( () => {btn_right.classList.add("background_red")}, 50)
+        btn_right.classList.remove("background_red")
         let element_to_move = movies.splice(0, 1)
         movies.push(element_to_move[0])
         return dispatch(movies, cat)
@@ -153,43 +157,43 @@ function listen_click(id) {
 }
 
 function show(movie) {
-    let modal_content = document.getElementById('modal_content')
-    modal_content.innerHTML = ''
-    span = document.createElement('span')
-    span.classList.add('close')
-    span.innerHTML = '&times;'
-    span.style.alignSelf = 'flex-end'
+    let modal_content = document.getElementById("modal_content")
+    modal_content.innerHTML = ""
+    span = document.createElement("span")
+    span.classList.add("close")
+    span.innerHTML = "&times;"
+    span.style.alignSelf = "flex-end"
     modal_content.appendChild(span)
-    image = document.createElement('img')
+    image = document.createElement("img")
     image.src = movie.image_url;
-    image.classList.add('bigger_image')
-    image.style.alignSelf = 'center'
+    image.classList.add("bigger_image")
+    image.style.alignSelf = "center"
     modal_content.appendChild(image)
-    paragraph = document.createElement('p')
-    paragraph.style.textAlign = 'left'
+    paragraph = document.createElement("p")
+    paragraph.style.textAlign = "left"
     if (movie.usa_gross_income == null) {
-        movie.usa_gross_income = 'inconnu'
+        movie.usa_gross_income = "inconnu"
     }
-    paragraph.innerHTML = 'Titre du film : ' + movie.title + '<br><br>' +
-                          'Genres: ' + movie.genres + '<br><br>' +
-                          'Date de sortie: ' + movie.date_published + '<br><br>' +
-                          'Rated: ' + movie.rated + '<br><br>' +
-                          'Score IMDB: ' + movie.imdb_score + '<br><br>' +
-                          'Réalisateur: ' + movie.directors + '<br><br>' +
-                          'Liste des acteurs: ' + movie.actors + '<br><br>' +
-                          'Durée: ' + movie.duration + ' minutes' + '<br><br>' +
-                          'Pays d\'origine: ' + movie.countries + '<br><br>' +
-                          'Résultat au Box-Office en USD: ' + movie.usa_gross_income +'<br><br>' +
-                          'Résumé du film: ' + movie.long_description + '<br><br>'
+    paragraph.innerHTML = "Titre du film : " + movie.title + "<br><br>" +
+                          "Genres: " + movie.genres + "<br><br>" +
+                          "Date de sortie: " + movie.date_published + "<br><br>" +
+                          "Rated: " + movie.rated + "<br><br>" +
+                          "Score IMDB: " + movie.imdb_score + "<br><br>" +
+                          "Réalisateur: " + movie.directors + "<br><br>" +
+                          "Liste des acteurs: " + movie.actors + "<br><br>" +
+                          "Durée: " + movie.duration + " minutes" + "<br><br>" +
+                          "Pays d\'origine: " + movie.countries + "<br><br>" +
+                          "Résultat au Box-Office en USD: " + movie.usa_gross_income +"<br><br>" +
+                          "Résumé du film: " + movie.long_description + "<br><br>"
     modal_content.appendChild(paragraph)
-    let modal = document.getElementById('modal')
-    modal.style.display = 'flex'
+    let modal = document.getElementById("modal")
+    modal.style.display = "flex"
     listen_close(modal)
 }
 
 function listen_close(modal) {
-    let close = document.getElementsByTagName('span')[0]
+    let close = document.getElementsByTagName("span")[0]
     close.onclick = () => {
-        modal.style.display = 'none'
+        modal.style.display = "none"
     }
 }
